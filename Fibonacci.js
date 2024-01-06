763aed0c52da34c0714edef3d7ff23aa76839721
lucca-software-infnet <123994038+lucca-software-infnet@users.noreply.github.com>
2024-01-06 16:41:37 -0300

    Initial commit

---
+++ Fibonacci.js
@@ -0,0 +1,16 @@
+let soma1 = 0;
+let soma2 = 1;
+
+do {
+    const tempo = soma2
+
+  soma2 = soma1 + soma2 
+  soma1 = tempo 
+ console.log(`${soma1} + ${soma2} = ${soma1 + soma2}`);
+  
+}while(soma2 <= 21)
+
+
+
+
+
---
+++ soma-Fibonacci.js
@@ -0,0 +1,20 @@
+
+let soma1 = 0;
+let soma2 = 1;
+let contador = 1;
+let parametro = 20;
+
+while( contador <= parseInt(parametro)){
+contador++
+  const tempo = soma2
+
+  soma2 = soma1 + soma2 
+  soma1 = tempo 
+
+  if( contador === (parametro + 1) ){
+    
+ console.log(`${soma1} + ${soma2} = ${soma1 + soma2}`);
+    
+  }
+
+}
